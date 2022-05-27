8b9a244be6b36fafc99c50bfb024c89a90b392df
HeatherH <hherries@yahoo.com>
2022-05-27 13:48:15 -0600

    Created ability to delete and filter tasks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Define UI Variables
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-task");
+const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
@@ -11,12 +11,18 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener("submit", addTask);
+  //Remove task event
+  taskList.addEventListener("click", removeTask);
+  // Clear task even
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
 
   // Add Task
   // The following function will create an alert if the "Add Task" button is clicked with no value.
   function addTask(e) {
     if (taskInput.value === "") {
-      alert("Add a task");
+      alert("Please add a task.");
     }
 
     // Create new li element
@@ -42,4 +48,38 @@ function loadEventListeners() {
 
     e.preventDefault();
   }
+}
+
+// Remove Task Function
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure want to remove this Task?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Task Function
+function clearTasks() {
+  // One option
+  // taskList.innerHTML = "";
+
+  // More efficient option
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks Function
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
