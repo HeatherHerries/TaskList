7d66e3863d2493470689f842e3485d917501443d
HeatherH <hherries@yahoo.com>
2022-05-27 17:39:38 -0600

    Gave ability to save to local storage to finish

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
-// Function to load all event listeners
+// Load all event listeners
 loadEventListeners();
 
 function loadEventListeners() {
+  // DOM load event
+  document.addEventListener("DOMContentLoaded", getTasks);
   // Add task event
   form.addEventListener("submit", addTask);
   //Remove task event
@@ -17,6 +19,33 @@ function loadEventListeners() {
   clearBtn.addEventListener("click", clearTasks);
   // Filter tasks event
   filter.addEventListener("keyup", filterTasks);
+
+  // Get Tasks from Local Storage
+  function getTasks() {
+    if (localStorage.getItem("tasks") === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function(task) {
+      const li = document.createElement("li");
+
+      li.className = "collection-item";
+
+      li.appendChild(document.createTextNode(task));
+
+      const link = document.createElement("a");
+
+      link.className = "delete-item secondary-content";
+
+      link.innerHTML = '<i class="fa fa-remove"></i>';
+
+      li.appendChild(link);
+
+      taskList.appendChild(li);
+    });
+  }
 
   // Add Task
   // The following function will create an alert if the "Add Task" button is clicked with no value.
@@ -43,6 +72,9 @@ function loadEventListeners() {
     // Append li to ul
     taskList.appendChild(li);
 
+    //Store data in local storage
+    storeTaskInLocalStorage(taskInput.value);
+
     // Clear the input
     taskInput.value = "";
 
@@ -50,13 +82,48 @@ function loadEventListeners() {
   }
 }
 
+// Store Task Function
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 // Remove Task Function
 function removeTask(e) {
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("Are you sure want to remove this Task?")) {
       e.target.parentElement.parentElement.remove();
+
+      // Remove from Local Storage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
+}
+
+// Remove from Local Storage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function(task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Clear Task Function
@@ -67,6 +134,14 @@ function clearTasks() {
   // More efficient option
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
+  }
+
+  // Clear from Local Storage
+  clearTasksFromLocalStorage();
+
+  // Clear tasks from Local Storage function
+  function clearTasksFromLocalStorage() {
+    localStorage.clear();
   }
 }
 
